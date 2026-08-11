9c4fa1ea6a4d3aa2f8a20e191a77a3aae6fb85a0
Tiago França <github@tiagofranca.com>
2026-08-11 12:57:45 -0300

    prettier and style fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+    compatibilityDate: '2025-07-15',
+    devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/icon',
-    '@nuxtjs/algolia',
-    '@nuxtjs/color-mode',
-    '@nuxtjs/i18n',
-    '@nuxtjs/seo',
-    '@nuxtjs/tailwindcss',
-    '@oro.ad/nuxt-claude-devtools'
-  ]
-})
+    modules: [
+        '@nuxt/icon',
+        '@nuxtjs/algolia',
+        '@nuxtjs/color-mode',
+        '@nuxtjs/i18n',
+        '@nuxtjs/seo',
+        '@nuxtjs/tailwindcss',
+        '@oro.ad/nuxt-claude-devtools',
+    ],
+});
