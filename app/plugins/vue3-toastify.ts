262efde5898d28bea9b79ae7d42c83cc022ad4c1
Tiago França <github@tiagofranca.com>
2026-08-15 22:15:38 -0300

    fix: resolve tailwind v4 CSS scoped style conflicts in CodeBlock

--- app/plugins/vue3-toastify.ts
+++ app/plugins/vue3-toastify.ts
@@ -1,17 +1,10 @@
-import { toast as createToast } from 'vue3-toastify';
+import { toast } from 'vue3-toastify';
 
 export default defineNuxtPlugin((nuxtApp) => {
-    const toast = (message: string, options?: any) => {
-        return createToast(message, {
+    nuxtApp.provide('toast', (message: string, options?: any) => {
+        toast(message, {
             autoClose: 2000,
-            position: 'bottom-right',
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            theme: 'colored',
             ...options,
         });
-    };
-
-    nuxtApp.provide('toast', toast);
+    });
 });
